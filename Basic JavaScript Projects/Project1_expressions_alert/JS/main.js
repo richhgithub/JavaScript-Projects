186660848317ddb073090cdde0accd19855ab0e4
richhgithub <richhalloran@gmail.com>
2021-03-21 21:41:55 -0400

    commit_step63

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+window.alert("Hello, world!"); //utilize window.alert() method
+document.write("Hello, world!"); //utilze document.write() method
+var Daughter = "Penny"; //assign string to variable
+var Son = "Zorro"; //assign string to variable
+var Kids = Daughter + Son; //concatenate the variables
+var Daughter = Daughter.fontcolor("blue"); //assign color to variable Daughter
+1 + 1 //an expression
