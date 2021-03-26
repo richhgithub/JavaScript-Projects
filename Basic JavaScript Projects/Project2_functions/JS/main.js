e6301000da19689b7ae164a9fb0af2dfd68773a5
richhgithub <richhalloran@gmail.com>
2021-03-25 22:00:43 -0400

    commit_step_68

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function myFunction() { //create a function
+    var sentence = "I am learning"; //create var
+    sentence += " a lot from this book!"; //concatenate a string using the += operator
+    document.getElementById("Concatenate").innerHTML = sentence; //display the concatenated string
 }
