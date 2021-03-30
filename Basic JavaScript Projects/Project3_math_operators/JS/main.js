5b3bb455758d3a320c07579cac130f63699332f3
richhgithub <richhalloran@gmail.com>
2021-03-29 22:32:47 -0400

    commit_math

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -5,5 +5,15 @@ document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 
 function subtraction_Function() {
     var Subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication()   {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
 }
