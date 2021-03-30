32c20ca12a56bd3bb61e4073ed7da34047c3710d
richhgithub <richhalloran@gmail.com>
2021-03-29 22:13:20 -0400

    commit_add_subtract

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function addition_Function( ) {
 var addition = 2 + 2;
 document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+}
