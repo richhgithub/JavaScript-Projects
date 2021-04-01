cc278874b2aa0f1513181d727fdf5b02a99d5711
richhgithub <richhalloran@gmail.com>
2021-03-31 21:18:14 -0400

    commit_step84

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -17,3 +17,9 @@ function division() {
     var simple_Math = 48 / 6;
     document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
 }
+
+function more_Math()    {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10,
+    divided in half and then subtracted by 5 equals " + simple_Math;
+}
