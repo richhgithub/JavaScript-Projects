a381419ab06728ee5dd0bfa3f4c623c67da1de00
richhgithub <richhalloran@gmail.com>
2021-04-06 13:12:23 -0400

    commit_step94

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,47 +1,55 @@
-function addition_Function( ) {
+function addition_Function( ) { //addition
 var addition = 2 + 2;
 document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
 
-function subtraction_Function() {
+function subtraction_Function() {   //subtraction
     var Subtraction = 5 - 2;
     document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
 }
 
-function multiplication()   {
+function multiplication()   {   //multiplication
     var simple_Math = 6 * 8;
     document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math;
 }
 
-function division() {
+function division() {   //division
     var simple_Math = 48 / 6;
     document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
 }
 
-function more_Math()    {
+function more_Math()    {   //math
     var simple_Math = (1 + 2) * 10 / 2 -5;
     document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10, "+
     "divided in half and then subtracted by 5 equals " + simple_Math;
 }
 
-function modulus_Operator() {
+function modulus_Operator() {   //modulus
     var simple_Math = 25 % 6;
     document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
 }
 
-function negation_Operator()    {
+function negation_Operator()    {   //negation
     var x = 10;
     document.getElementById("Math7").innerHTML = -x;
 }
 
-function increment_Operator()   {
+function increment_Operator()   {   //increment
     var X =5;
     X++;
     document.write(X);
 }
 
-function decrement_Operator()   {
+function decrement_Operator()   {   //decrement
     var Y = 5.25;
     Y--;
     document.write(Y);
+}
+
+window.alert(Math.random());    //random 
+
+window.alert(Math.random() * 100);  //random
+
+function round_Operator() { //round
+document.getElementById("demo").innerHTML = Math.round(4.4);
 }
