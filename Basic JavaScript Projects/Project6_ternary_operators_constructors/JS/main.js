04871ee13c3cc3d91d75b0385ca7c2c79c9a4c56
richhgithub <richhalloran@gmail.com>
2021-04-11 17:10:56 -0400

    commit_step139

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,24 +1,24 @@
 function Ride_Function()    {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";   //ternary syntax
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
 function Vote_Function()    {
     var Age, Can_vote;
     Age = document.getElementById("Age").value;
-    Can_vote = (Age < 18) ? "You are not old enough to vote.":"You can vote!";
+    Can_vote = (Age < 18) ? "You are not old enough to vote.":"You can vote!";  //ternary syntax
     document.getElementById("Vote").innerHTML = Can_vote;
 }
 
-function Vehicle(Make, Model, Year, Color)  {   //step 133 and the Class and utilize the new keyword
-    this.Vehicle_Make = Make;
+function Vehicle(Make, Model, Year, Color)  {   //step 133 and the Class and new keyword and this
+    this.Vehicle_Make = Make;   //properties
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");  //instances
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction()   {   //the function
