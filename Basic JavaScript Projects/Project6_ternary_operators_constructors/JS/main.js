f5c1f8195851a338bfbb9c364f0f682f23c7197b
richhgithub <richhalloran@gmail.com>
2021-04-11 17:03:11 -0400

    commit_step138

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -11,3 +11,26 @@ function Vote_Function()    {
     Can_vote = (Age < 18) ? "You are not old enough to vote.":"You can vote!";
     document.getElementById("Vote").innerHTML = Can_vote;
 }
+
+function Vehicle(Make, Model, Year, Color)  {   //step 133 and the Class and utilize the new keyword
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction()   {   //the function
+    document.getElementById("New_and_This").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function count_Function()   {   //step 138
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count()    {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
