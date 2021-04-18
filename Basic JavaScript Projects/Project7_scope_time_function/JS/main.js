05a27dfda63cb5b0def8c4d5a127e9b875f10263
richhgithub <richhalloran@gmail.com>
2021-04-17 23:46:00 -0400

    commit_step151

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -33,7 +33,42 @@ Add_numbers_2();
 
 //Date().getHours() example
 function get_Date() {
-    if (new Date().getHours() < 18) {
+    if (new Date().getHours() < 23) {
     document.getElementById("Greeting").innerHTML = "How are you today?";
     }
+}
+
+//if statement
+function get_Date_2() {
+    if (new Date().getHours() < 23) {
+    document.getElementById("Greeting_2").innerHTML = "Go to bed.";
+    }
+}
+
+//else example
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18)  {
+        Vote = "You are old enough to vote!";
+    }
+    else    {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+//else if example
+function Time_function()    {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0)  {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18)   {
+        Reply = "It is afternoon.";
+    }
+    else    {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
