cfa7162b8c58377134328dbc568a00cda874c3bd
richhgithub <richhalloran@gmail.com>
2021-04-24 19:41:45 -0400

    commit_cat

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -7,3 +7,22 @@ function Call_Loop()    {
     }
     document.getElementById("Loop").innerHTML = Digit;
 }
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop()    {
+    for (Y = 0; Y < Instruments.length; Y++)    {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_Function()   {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Array").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
+}
