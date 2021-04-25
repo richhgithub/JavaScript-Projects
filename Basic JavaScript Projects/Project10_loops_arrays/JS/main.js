d29683ca764953f358430ff9641bb49910383ef0
richhgithub <richhalloran@gmail.com>
2021-04-24 21:06:47 -0400

    commit_step_214

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -26,3 +26,19 @@ function array_Function()   {
     Cat_Picture[3] = "purring";
     document.getElementById("Array").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
 }
+
+function constant_function()    {
+    const Musical_Instrument = {type:"guiter", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.brand = "Gibson";
+    Musical_Instrument.price = "$800";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price + " for a " + Musical_Instrument.brand;
+}
+
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
