d9b213cbf444f65af4b8e1d3b6c4cfb6be77cfa3
richhgithub <richhalloran@gmail.com>
2021-04-26 23:18:22 -0400

    commit_js

--- Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_1.js
+++ Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_1.js
@@ -34,3 +34,11 @@ function Hello_World_Function() {
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d"); ctx.strokeStyle = "#FF0000"; ctx.strokeRect(20, 20, 150, 100);
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
