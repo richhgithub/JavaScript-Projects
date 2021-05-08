c2006df3ae5656fca312ee09443e6340921573d5
richhgithub <richhalloran@gmail.com>
2021-05-07 20:45:07 -0400

    Update Pizza.js

--- Basic JavaScript Projects/JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Basic JavaScript Projects/JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -19,7 +19,7 @@ function getReceipt()   {
         sizeTotal = 10;
     } else if (selectedSize === "Large Pizza")  {
         sizeTotal = 14;
-    } else if (selectedSize === "Extra Larger Pizza")   {
+    } else if (selectedSize === "Extra Large Pizza")   {
         sizeTotal = 16;
     }
     runningTotal = sizeTotal;
